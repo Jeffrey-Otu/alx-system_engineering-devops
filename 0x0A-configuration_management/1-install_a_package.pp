bde3e8fca38554fb2f329d6caa4582a63bb4bfcd
Jeffrey-Otu <otujeffrey02@gmail.com>
2023-06-24 05:33:05 -0700

    correction

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,7 @@
 # pip3 install flask -v 2.1.0
 
-exec { 'pip3 install flask':
-  command => '/usr/local/lib/python3.4/dist-packages/flask -v 2.1.0',
-}
+exec { 'pip3 install flask -v 2.1.0':
+     command   => 'pip3 install flask flask_restful apiai',
+     path => ['/usr/bin/'],
+     unless  => '/usr/bin/test -f /usr/local/lib/python3.4/dist-packages/flask/app.py'
+  }
